e95f96dc1bfbe8eadb192994f0e77b42155b086a
Algirwitch <olia93@mail.ru>
2017-11-16 00:32:43 +0300

    Добавлен вспомогательный параметр

---
+++ contact.py
@@ -0,0 +1,9 @@
+
+class Contact:
+
+    def __init__(self, firstname, lastname, address, mobile, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.mobile = mobile
+        self.email = email
